148d47a61d317b1f240c79a50283442a0a3e0be0
Mateus Pevidor <mateus-pevidor@hotmail.com>
2022-08-11 01:02:22 -0300

    feat: matrix multiplication

--- Assets/MatrixMult/MatrixMult.cs
+++ Assets/MatrixMult/MatrixMult.cs
@@ -7,37 +7,74 @@ public class MatrixMult : MonoBehaviour
 
     public ComputeShader computeShader;
 
-    private List<int> A;
-    private List<int> B;
+    public float startTime, endTime;
 
-    float startTime, endTime;
+    private List<int> _A, _B;
+
+    public int size;
+    public int nThreads;
 
     void Start()
     {
-        int size = (int) Mathf.Pow(2, 10); // 65.536
-        int numThreads = 64 * 1 * 1;
+        SumArray sumArray = GetComponent<SumArray>();
 
-        int[] result = new int[0];
-        A = new List<int>();
-        B = new List<int>();
-        
-        
+        _A = new List<int>();
+        _B = new List<int>();
+
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
-                A.Add(Random.Range(0, 10));
+                _A.Add(j + i * size);
+                // A.Add(Random.Range(0, 10));
             }
 
-            B.Add(Random.Range(0, 10));
+            _B.Add(i);
+            // B.Add(Random.Range(0, 10));
         }
 
         startTime = Time.realtimeSinceStartup;
+
+        List<int> _y = Mult(_A, _B);
+
+        List<int> result = new List<int>();
+
+        for (int i = 0; i < size; i++) {
+            List<int> row = _y.GetRange(i * size, size);
+            int r = sumArray.Sum(row, nThreads);
+            result.Add(r);
+        }
+
+        endTime = Time.realtimeSinceStartup;
+        Debug.Log("[MatMult] Time: " + (endTime - startTime));
+
+    }
+
+    public List<int> Mult(List<int> A, List<int> B) {
+        int size = B.Count; // 65.536
+        int numThreads = nThreads * 1 * 1;
+
+        int[] result = new int[0];
+        // A = new List<int>();
+        // B = new List<int>();
+        
+        
+        // for (int i = 0; i < size; i++) {
+        //     for (int j = 0; j < size; j++) {
+        //         A.Add(j + i * size);
+        //         // A.Add(Random.Range(0, 10));
+        //     }
+
+        //     B.Add(i);
+        //     // B.Add(Random.Range(0, 10));
+        // }
+
+        // startTime = Time.realtimeSinceStartup;
 
         int rowsByThread = size / numThreads;
 
         ComputeBuffer A_Buffer = new ComputeBuffer(size * size, sizeof(int));
         ComputeBuffer B_Buffer = new ComputeBuffer(size, sizeof(int));
 
-        ComputeBuffer resultBuffer = new ComputeBuffer(size, sizeof(int));
+        ComputeBuffer resultBuffer = new ComputeBuffer(size * size, sizeof(int));
 
         A_Buffer.SetData(A.ToArray());
         B_Buffer.SetData(B.ToArray());
@@ -49,18 +86,20 @@ public class MatrixMult : MonoBehaviour
         computeShader.SetBuffer(0, "result", resultBuffer);
         computeShader.Dispatch(0, numThreads, 1, 1);
 
-        result = new int[size];
+        result = new int[size * size];
         resultBuffer.GetData(result);
 
         A_Buffer.Dispose();
         B_Buffer.Dispose();
         resultBuffer.Dispose();
 
-        endTime = Time.realtimeSinceStartup;
+        // endTime = Time.realtimeSinceStartup;
 
 
-        Debug.Log("Time: " + (endTime - startTime));
-        for (int i = 0; i < result.Length; i++)
-            Debug.Log("Result: " + result[i]);
+        // Debug.Log("[MatMult] Time: " + (endTime - startTime));
+        // for (int i = 0; i < result.Length; i++)
+            // Debug.Log("Result: " + result[i]);
+        
+        return new List<int>(result);
     }
 }
--- Assets/SumArray/SumArray.cs
+++ Assets/SumArray/SumArray.cs
@@ -7,50 +7,59 @@ public class SumArray : MonoBehaviour
 
     public ComputeShader computeShader;
 
-    private List<int> numbers;
+    // public List<int> numbers;
 
-    float startTime, endTime;
+    public float startTime, endTime;
 
     void Start()
     {
+        // Sum(numbers, 4);
+    }
+
+    public int Sum(List<int> arr, int nThreads) {
+        List<int> array = new List<int>(arr);
+
         int numbersCount = (int) Mathf.Pow(2, 23); // 8.388.608
-        int numThreads = 64 * 1 * 1;
+        int numThreads = nThreads * 1 * 1;
 
         int[] result = new int[0];
-        numbers = new List<int>();
+        // array = new List<int>();
         
-        for (int i = 0; i < numbersCount; i++) {
-            numbers.Add(Random.Range(0, 10));
-        }
+        // for (int i = 0; i < numbersCount; i++) {
+        //     array.Add(Random.Range(0, 10));
+        // }
+
+        ComputeBuffer numbersBuffer = new ComputeBuffer(numbersCount, sizeof(int));
+        ComputeBuffer numbersBufferAux = new ComputeBuffer(numThreads, sizeof(int));
+        computeShader.SetBuffer(0, "result", numbersBufferAux);
 
         startTime = Time.realtimeSinceStartup;
 
         while (numbersCount > 1) {
-            ComputeBuffer numbersBuffer = new ComputeBuffer(numbersCount, sizeof(int));
-            ComputeBuffer numbersBufferAux = new ComputeBuffer(numThreads, sizeof(int));
-
-            numbersBuffer.SetData(numbers.ToArray());
+            numbersBuffer.SetData(array.ToArray());
 
             computeShader.SetFloat("k", numbersCount / numThreads);
             computeShader.SetBuffer(0, "numbers", numbersBuffer);
-            computeShader.SetBuffer(0, "result", numbersBufferAux);
             computeShader.Dispatch(0, numThreads, 1, 1);
 
             result = new int[numThreads];
             numbersBufferAux.GetData(result);
 
-            numbers = new List<int>(result);
+            array = new List<int>(result);
 
             numbersCount = result.Length;
             numThreads /= 2;
-
-            numbersBuffer.Dispose();
-            numbersBufferAux.Dispose();
         }
 
         endTime = Time.realtimeSinceStartup;
 
-        Debug.Log("Time: " + (endTime - startTime));
-        Debug.Log("Result: " + result[0]);
+
+        numbersBuffer.Dispose();
+        numbersBufferAux.Dispose();
+
+        // Debug.Log("[SumArr] Time: " + (endTime - startTime));
+        // Debug.Log("Result: " + result[0]);
+
+        return result[0];
     }
 }
